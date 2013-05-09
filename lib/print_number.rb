e5ed5df52637c431375bc68eaf7bbcfa972668ea
Faisal Niaz <faisal.niaz@gmail.com>
2013-05-10 00:12:13 +0100

    Refactored code to make it more generic Cleaned code (code is now quite close to final form)

--- lib/print_number.rb
+++ lib/print_number.rb
@@ -3,17 +3,25 @@ module PrintNumber
 
   def print(input_number)
     @number = input_number
-    convert_to_text
-  end
 
-  def convert_to_text
-    send "text_for_#{number_of_digits}_digit_number"
+    blocks = three_digit_blocks
+    text = ''
+    blocks.each_with_index do |b, i|
+      @number = b.to_i
+      text += "#{convert_to_text}#{block_names[blocks.length - i -1]}" unless (@number == 0 && blocks.length > 1)
+    end
+
+    text
   end
 
 
   private
 
-  # number processor methods
+  # processor methods
+  def convert_to_text
+    send "text_for_#{number_of_digits}_digit_number"
+  end
+
   def text_for_1_digit_number
     digit_text[number]
   end
@@ -22,29 +30,37 @@ module PrintNumber
     if number % 10 == 0
       tens_text[number / 10]
     elsif (11..19).include? number
-      eleven_nineteen[number - 10 - 1]
+      eleven_nineteen[number - 11]
     else
       "#{tens_text[number / 10]} #{digit_text[number % 10]}"
     end
   end
 
   def text_for_3_digit_number
-    "#{digit_text[digit(1)]} #{hundred}" + (number % 100 == 0 ? "" : " and #{print(chop_head)}")
-  end
-
-  def text_for_4_digit_number
-    "#{digit_text[digit(1)]} Thousand"
-  end
-
-  def text_for_5_digit_number
-    "#{print(get_head(2))} Thousand"
-  end
-
-  def text_for_6_digit_number
-    "#{print(get_head(3))} Thousand"
+    "#{digit_text[digit(1)]} Hundred" + (number % 100 == 0 ? "" : " and #{print(chop_head)}")
   end
 
   #utility methods
+  def three_digit_blocks
+    blocks = []
+
+    if digits.length < 4
+      blocks << digits
+      return blocks
+    end
+
+    block = ""
+    s = digits.reverse
+    for i in 0...s.length
+      block += s[i]
+      if (i + 1) % 3 == 0
+        blocks << block.reverse
+        block = ""
+      end
+    end
+    blocks << block.reverse
+    blocks.reverse
+  end
 
   def digits
     number.to_s
@@ -54,23 +70,16 @@ module PrintNumber
     digits.length
   end
 
-  # digit number if 1 based, as used in regular English
+  # digit number is 1 based, as used in regular English
   def digit(digit_number)
     digits[digit_number - 1].to_i
   end
 
   def chop_head
-    digits.reverse.chop.reverse.to_i
+    digits[1...digits.length].to_i
   end
 
-  def get_head(number_of_digits)
-    digits[0...number_of_digits].to_i
-  end
-
-  def get_tail(number_of_digits)
-    digits[(digits.length - number_of_digits)...digits.length].to_i
-  end
-
+  # data
   def digit_text
     @digit_text ||= %w[Zero One Two Three Four Five Six Seven Eight Nine]
   end
@@ -80,14 +89,10 @@ module PrintNumber
   end
 
   def eleven_nineteen
-    eleven_nineteen= %w[Eleven Twelve Thirteen Fourteen Fifteen Sixteen Seventeen Eighteen Nineteen]
+    @eleven_nineteen ||= %w[Eleven Twelve Thirteen Fourteen Fifteen Sixteen Seventeen Eighteen Nineteen]
   end
 
-  def hundred
-    'Hundred'
-  end
-
-  def number
-    @number
+  def block_names
+    ['', ' Thousand', ' Million']
   end
 end

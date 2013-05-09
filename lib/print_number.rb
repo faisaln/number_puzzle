7fd7f0fbc862aa91092779d851d1d654fa0ce65f
Faisal Niaz <faisal.niaz@gmail.com>
2013-05-09 13:37:40 +0100

    Adds first test prints 'One'

--- lib/print_number.rb
+++ lib/print_number.rb
@@ -1,3 +1,5 @@
 module PrintNumber
-
+  def print(number)
+    'One'
+  end
 end
--- spec/lib/print_number_spec.rb
+++ spec/lib/print_number_spec.rb
@@ -1,5 +1,9 @@
 require 'spec_helper'
 
 describe PrintNumber do
+  include PrintNumber
 
+  context 'single digits' do
+    specify {print(1).should == "One"}
+  end
 end

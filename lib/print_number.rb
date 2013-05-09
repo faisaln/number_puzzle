5c12a2a9342baaa44349b180b126f1f669b77209
Faisal Niaz <faisal.niaz@gmail.com>
2013-05-09 16:39:43 +0100

    prints text for tens

--- lib/print_number.rb
+++ lib/print_number.rb
@@ -1,6 +1,6 @@
 module PrintNumber
   def print(number)
-    digit_text[number]
+    digits(number) == 2 ? tens_text[number / 10] : digit_text[number]
   end
 
   private
@@ -10,6 +10,10 @@ module PrintNumber
   end
 
   def digit_text
-    @digit_text ||= %w[Zero One Two Three Four Five Six Seven Eight Nine Ten]
+    @digit_text ||= %w[Zero One Two Three Four Five Six Seven Eight Nine]
+  end
+
+  def tens_text
+    @tens_text ||= %w[Zero Ten Twenty Thirty Forty Fifty Sixty Seventy Eighty Ninety]
   end
 end
--- spec/lib/print_number_spec.rb
+++ spec/lib/print_number_spec.rb
@@ -8,14 +8,29 @@ describe PrintNumber do
     digits(33).should be(2)
     digits(3377).should be(4)
     digits(0000).should_not be(4)
+    digits(0000).should be(1)
+    digits(20).should be(2)
+    digits(300).should be(3)
   end
 
   context 'single digits' do
-    digit_text = %w[Zero One Two Three Four Five Six Seven Eight Nine Ten]
+    digit_text = %w[Zero One Two Three Four Five Six Seven Eight Nine]
 
     it 'print text for each digit' do
       for i in 0...digit_text.length
         print(i).should == digit_text[i]
+      end
+    end
+  end
+
+  context '2 digit numbers' do
+    context 'tens' do
+      tens_text = %w[Ten Twenty Thirty Forty Fifty Sixty Seventy Eighty Ninety]
+
+      it 'prints text for tens' do
+        for i in 1...tens_text.length
+          print(i * 10).should == tens_text[i - 1]
+        end
       end
     end
   end
